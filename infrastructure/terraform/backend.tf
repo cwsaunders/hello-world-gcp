7812a1599f9182a21ccfbf9442360495dd4e40e2
cwsaunders <csaunders5@liberty.edu>
2025-04-06 01:08:17 -0400

    pipeline run

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket      = " terraform-state-2944042"  
+    bucket      = "terraform-state-2944042"  
     prefix      = "terraform/state"            
   }
 }
